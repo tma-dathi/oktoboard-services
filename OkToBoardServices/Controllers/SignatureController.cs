3a73bfeb6b0a217bc0e3427e513738f2f34422d6
Thi Duong <dathi.me@outlook.com>
2015-07-28 08:06:17 +0700

    refractoring code, add 'GET' function

--- OkToBoardServices/Controllers/SignatureController.cs
+++ OkToBoardServices/Controllers/SignatureController.cs
@@ -9,28 +9,45 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using OkToBoardServices.Models;
+using System.Net.Http.Headers;
 
 namespace OkToBoardServices.Controllers
 {
     [RequireHttps]
     public class SignatureController : ApiController
     {
-         DBContext db = new DBContext();
-        // GET api/signature
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value3"};
-        }
-
-        private void HandleNonHttpsRequest(HttpActionContext actionContext)
-        {
-            throw new NotImplementedException();
-        }
-
+        DBContext db = new DBContext();
+        
         // GET api/signature/5
-        public string Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            return "value";
+            string dir = HttpContext.Current.Server.MapPath(String.Format(@"~\Images\Signatures\{0}", id));
+            var image = db.Reports.Where(x => x.Id == id).Select(y => y.Image).First();
+            var httpResponseMessage = new HttpResponseMessage();
+            var memoryStream = new MemoryStream();
+            FileStream fileStream = null;
+            try
+            {
+                using (fileStream = File.OpenRead(image))
+                {
+                    fileStream.CopyTo(memoryStream);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.log.Error("Error while read zip file and write to stream: " + ex.InnerException.ToString());
+            }
+            finally
+            {
+                if (fileStream != null) fileStream.Close();
+            }
+
+            httpResponseMessage.Content = new ByteArrayContent(memoryStream.ToArray());
+            httpResponseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            httpResponseMessage.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            httpResponseMessage.Content.Headers.ContentDisposition.FileName = System.IO.Path.GetFileName(image);
+            httpResponseMessage.StatusCode = HttpStatusCode.OK;
+            return httpResponseMessage;
         }
 
         // POST api/signature
@@ -42,66 +59,56 @@ namespace OkToBoardServices.Controllers
                 var userId = int.Parse(HttpContext.Current.Request.Headers.Get("otb-userid"));
                 var filename = HttpContext.Current.Request.Headers.Get("otb-filename");
                 string dir = HttpContext.Current.Server.MapPath(String.Format(@"~\Images\Signatures\{0}", userId));
-                //string path = String.Format(@"{0}\{1}", dir, filename);
-                var action = int.Parse(HttpContext.Current.Request.Headers.Get("action"));
-                foreach (var h in HttpContext.Current.Request.Headers.AllKeys)
+                string path = String.Format(@"{0}\{1}", dir, filename);
+                if (filename != null)
                 {
-                    Logger.log.Debug(h);
-                }
-                //1: Create new, 2:Update, 3:Delete
-                if (action == 1)
-                {
-                    string path = "";
-                    if (filename != null)
+                    Directory.CreateDirectory(dir);
+                    using (FileStream output = File.OpenWrite(path))
                     {
-                        Directory.CreateDirectory(dir);
-                        using (FileStream output = File.OpenWrite(path))
-                        {
-                            input.CopyTo(output);
-                        }
-                         path = String.Format(@"{0}\{1}", dir, filename);
+                        input.CopyTo(output);
                     }
                     var items = new Report { Id = userId, Image = path };
                     db.Reports.Add(items);
                     db.SaveChanges();
                     Logger.log.Debug("Save successful");
                 }
-                if (action == 2)
-                {
-                    if (File.Exists(dir))
-                    {
-                        var directoryInfor = new DirectoryInfo(dir);
-                        foreach (FileInfo file in directoryInfor.GetFiles()) file.Delete();
-                    }
-                    else
-                    {
-                        Directory.CreateDirectory(dir);    
-                    }
-                    if (filename != null)
-                    {
-                        var report = (from rp in db.Reports
-                                      where rp.Id == userId
-                                      select rp).First();
-                        string path = String.Format(@"{0}\{1}", dir, filename);
-                        using (FileStream output = File.OpenWrite(path))
-                        {
-                            input.CopyTo(output);
-                        }
-                        report.Image = path;
-                        db.SaveChanges();
-                    }
-                }
             }
             catch (Exception ex)
             {
-                Logger.log.Debug(ex);
+                Logger.log.Error(ex);
                 throw;
             }
         }
 
         // PUT api/signature/5
-        public void Put(int id, [FromBody]string value)
+        public void Put([FromBody]string value)
         {
+            Stream input = HttpContext.Current.Request.InputStream;
+            var userId = int.Parse(HttpContext.Current.Request.Headers.Get("otb-userid"));
+            var filename = HttpContext.Current.Request.Headers.Get("otb-filename");
+            string dir = HttpContext.Current.Server.MapPath(String.Format(@"~\Images\Signatures\{0}", userId));
+            if (File.Exists(dir))
+            {
+                var directoryInfor = new DirectoryInfo(dir);
+                foreach (FileInfo file in directoryInfor.GetFiles()) file.Delete();
+            }
+            else
+            {
+                Directory.CreateDirectory(dir);
+            }
+            if (filename != null)
+            {
+                var report = (from rp in db.Reports
+                              where rp.Id == userId
+                              select rp).First();
+                string path = String.Format(@"{0}\{1}", dir, filename);
+                using (FileStream output = File.OpenWrite(path))
+                {
+                    input.CopyTo(output);
+                }
+                report.Image = path;
+                db.SaveChanges();
+            }
         }
 
         // DELETE api/signature/5
